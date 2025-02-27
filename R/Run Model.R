7e026503479033a1a4d33dc29be1aa762f23b1f2
Dan Pollard <d.j.pollard@sheffield.ac.uk>
2025-02-27 17:53:26 +0000

    Created a function that will appropaitely run the model according to user settings (mostly in the Data/Global Options.csv file), and changed the Run Analyses.R file to be a script that will run through all model runs I need for a paper/report.

---
+++ R/Run Model.R
@@ -0,0 +1,64 @@
+
+#'@param parameter_, is the full parameters matrix
+#'@param pat_chars_, is the patient characteristics matrix
+#'@param Treatment_, is the variable that controls whether treatment effects are applied or not
+#'Intervention means the model is running an intervention arm. Anything else and intervention
+#'effects are not applied
+#'@param GlobalOptions_ is the data object containing all global options for the model
+#'e.g. discount rates, variables that control the number of patients, control variables 
+#'to determine if a scenario analysis is being conducted or not
+#'@param ResultsVariables_, is a list of all variables that I am collecting results for
+#'
+#'@return Results
+
+run_model <- function(parameter_,
+                      pat_chars_,
+                      Treatment_,
+                      GlobalOptions_,
+                      ResultsVariables_){
+  if(GlobalOptions_["Run_PSA","Value"]==FALSE){
+    #code to run deterministically
+    Results <- Single_model_run(1,
+                                parameter_,
+                                pat_chars_,
+                                Treatment_,
+                                GlobalOptions_,
+                                ResultsVariables_)
+  }else{
+    #Vector telling R which rows we of the parameter matrix we want to run in the PSA
+    #starts at 2, as row 1 is deterministic
+    #ends at the number of PSA iterations we want + 1, as we started at row 2
+    PSAvector <- 2:(as.numeric(GlobalOptions_["Number_of_PSARuns","Value"])+1)
+    
+    #set up parrallel processing
+    #these are the additional global variables that need to be set for parrallel
+    #processing functions
+    numCores <- detectCores()-1 #detect the number of cores on my current PC
+    #set the number of cores to use to be the minimum of the number in the GlobalOptions.csv 
+    #file or the number of detected cores on this PC. This is because the PC can crash if it 
+    #runs out of memory from running too many models at once.
+    numCores <- min (numCores, as.numeric(GlobalOptions_["Number_of_cores", "Value"])) 
+    
+    ##Next 3 lines of code set up clustering (technical thing needed for parallel processing)
+    cl <- makeCluster(numCores)
+    #Set a random number seed for parallel processing
+    clusterEvalQ(cl, set.seed(1))
+    
+    #Set up the cluster processing and 
+    #push all objects (data & user defined functions) in the global environment 
+    #to all the clusters
+    clusterExport(cl, ls(envir = .GlobalEnv))
+    
+    Results <- parSapply(cl = cl, #tell the parSapply what my clusters are for parrallel processing
+                              PSAvector, #note we now start on 2, as row 2 onwards as PSA samples! Here it is just so you can see that we are getting the same reuslts in sapply as running the function
+                              Single_model_run, #Function, but only the name
+                              parameter_ = Params, #Set each argument of the function to the right data
+                              pat_chars_ = pat_chars, #use the same notation as if we were running the function normally
+                              Treatment_ = Treatment,
+                              GlobalOptions_ = GlobalOptions,
+                              ResultsVariables_ = ResultsVariables)
+    
+    stopCluster(cl=cl)
+  }
+  return(Results)
+}
--- Run Analysis.R
+++ Run Analysis.R
@@ -19,19 +19,6 @@ colnames(pat_chars) <- Individual_variables
 ResultsVariables <- c("Life Expectancy", "Discounted Life Expectancy" ,"QALYs", 
                  "Discounted QALYs", "Costs", "Discounted Costs", "Mean Time Vert", "Mean Time Hip")
 
-#Temp variable for number of rows we will need, 1 if deterministic, 
-#1 for each PSA iteration if not
-
-Temp <- ifelse(GlobalOptions["Run_PSA", "Value"]==TRUE, 
-               as.numeric(GlobalOptions["Number_of_PSARuns","Value"]),
-                          1)
-
-#Set up the results matrix, which will store the model averaged results
-Results <- matrix(data = NA, nrow = Temp, ncol = length(ResultsVariables))
-
-###Assign names to the results matrix
-colnames(Results) <- ResultsVariables
-
 ####Step 1a: Set up a text variable to control whether we are using intervention or control
 Treatment <- "Baseline"
 ####Step 1b: Call in our user defined functions for this model
@@ -41,59 +28,56 @@ source("R/Deaths.R")
 source("R/Hip Fracture.R")
 source("R/Vert Fracture.R")
 source("R/Single Model Run.R")
+source("R/Run Model.R")
+##Base Case
+##Intervention
+BC_intervention <- run_model(Params,
+                             pat_chars,
+                             "Intervention",
+                             GlobalOptions,
+                             ResultsVariables)
 
-##PSA - single thread
-set.seed(1)
-start.time <- Sys.time()
-test_PSA <- sapply(2:17, #note we now start on 2, as row 2 onwards as PSA samples! Here it is just so you can see that we are getting the same reuslts in sapply as running the function
-                   Single_model_run, #Function, but only the name
-                     parameter_ = Params, #Set each argument of the function to the right data
-                     pat_chars_ = pat_chars, #use the same notation as if we were running the function normally
-                     Treatment_ = Treatment,
-                     GlobalOptions_ = GlobalOptions,
-                     ResultsVariables_ = ResultsVariables
-                   )
-end.time <- Sys.time()
-##Print run time
-time_psa_singlethread <- end.time - start.time
-time_psa_singlethread
-test_PSA
+write.csv(BC_intervention, "Results/BaseCaseResultsInterventionDet.csv")
+BC_control <- run_model(Params,
+                        pat_chars,
+                        "baseline",
+                        GlobalOptions,
+                        ResultsVariables)
+write.csv(BC_control, "Results/BaseCaseResultsControlDet.csv")
 
-#PSA - parallel processing
+##For real tests uncomment the next line of code - currently only doing 16 PSA runs
+#GlobalOptions["Number_of_PSARuns", "Value"] <- 1000
+GlobalOptions["Run_PSA", "Value"] <- TRUE
+BC_intervention <- run_model(Params,
+                             pat_chars,
+                             "Intervention",
+                             GlobalOptions,
+                             ResultsVariables)
 
+write.csv(BC_intervention, "Results/BaseCaseResultsInterventionPSA.csv")
+BC_control <- run_model(Params,
+                        pat_chars,
+                        "baseline",
+                        GlobalOptions,
+                        ResultsVariables)
+write.csv(BC_control, "Results/BaseCaseResultsControlPSA.csv")
 
-start.time <- Sys.time()
-#set up parrallel processing
-#these are the additional global variables that need to be set for parrallel
-#processing functions
-numCores <- detectCores()-1 #detect the number of cores on my current PC
-#set the number of cores to use to be the minimum of the number in the GlobalOptions.csv 
-#file or the number of detected cores on this PC. This is because the PC can crash if it 
-#runs out of memory from running too many models at once.
-numCores <- min (numCores, as.numeric(GlobalOptions["Number_of_cores", "Value"])) 
+##Assuming it is non-linear
+##Scenario 1.5% discounting
+GlobalOptions["Discount_rate_cost", "Value"] <- 0.015
+GlobalOptions["Discount_rate_QALY", "Value"] <- 0.015
 
-##Next 3 lines of code set up clustering (technical thing needed for parallel processing)
-cl <- makeCluster(numCores)
-#Set a random number seed for parallel processing
-clusterEvalQ(cl, set.seed(1))
+DR_1.5_intervention <- run_model(Params,
+                             pat_chars,
+                             "Intervention",
+                             GlobalOptions,
+                             ResultsVariables)
 
-#Set up the cluster processing and 
-#push all objects (data & user defined functions) in the global environment 
-#to all the clusters
-clusterExport(cl, ls(envir = .GlobalEnv))
+write.csv(DR_1.5_intervention, "Results/1.5percdiscInterventionPSA.csv")
 
-test_PSA_par <- parSapply(cl = cl, #tell the parSapply what my clusters are for parrallel processing
-                      2:17, #note we now start on 2, as row 2 onwards as PSA samples! Here it is just so you can see that we are getting the same reuslts in sapply as running the function
-                   Single_model_run, #Function, but only the name
-                   parameter_ = Params, #Set each argument of the function to the right data
-                   pat_chars_ = pat_chars, #use the same notation as if we were running the function normally
-                   Treatment_ = Treatment,
-                   GlobalOptions_ = GlobalOptions,
-                   ResultsVariables_ = ResultsVariables
-)
-end.time <- Sys.time()
-##Print run time
-time_psa_parallel <- end.time - start.time
-time_psa_parallel
-
-test_PSA_par
+DR_1.5_control <- run_model(Params,
+                        pat_chars,
+                        "baseline",
+                        GlobalOptions,
+                        ResultsVariables)
+write.csv(DR_1.5_control, "Results/1.5percdiscResultsControlPSA.csv")
